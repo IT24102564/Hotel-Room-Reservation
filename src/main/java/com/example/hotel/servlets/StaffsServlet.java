e39ace59de3059b248d05e816b44059adeebfc40
IT24103165 <it24103165@my.sliit.lk>
2025-05-21 08:35:50 +0530

    add StaffsServlet.java

---
+++ src/main/java/com/example/hotel/servlets/StaffsServlet.java
@@ -0,0 +1,14 @@
+package com.example.hotel.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name="StaffViewsServlet")
+public class StaffViewsServlet extends HttpServlet {
+    @Override
+    public  void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
